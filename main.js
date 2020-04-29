7e6c1ed8e753b2ccf322fd472cc71e71bdb77bfd
Beleks <53120159+Beleks@users.noreply.github.com>
2020-04-29 12:17:28 +0800

    0.0.1

---
+++ main.js
@@ -0,0 +1,39 @@
+var matr_1 = [
+    [2, 5, 1],
+    [0, 8, 4],
+    [3, 6, 9],
+    [1, 2, 7]
+]
+
+var matr_2 = [
+    [0, 7, 1],
+    [2, 8, 9],
+    [3, 3, 1]
+    // [6, 0, 4]
+]
+
+// console.log(matr)
+// console.log(matr[1].length)
+
+function multMatr(matr_1, matr_2){
+    var columMatr_1 = matr_1[0].length
+    var columMatr_2= matr_2[0].length
+    var lineMatr_1 = matr_1.length
+    var lineMatr_2 = matr_2.length
+    
+    var multMatr_3 = []
+    for (var y=0; y<lineMatr_1; y++){
+        multMatr_3[y] = [];
+    }
+    for (var k=0; k<columMatr_2; k++){
+        for(var i=0; i<lineMatr_1; i++){
+            var sum = 0
+            for (var l = 0; l<lineMatr_2; l++){
+                sum += matr_1[i][l]*matr_2[l][k]
+                multMatr_3[i][k] = sum
+            }
+        }
+    }
+    return multMatr_3
+}
+console.log(multMatr(matr_1,matr_2))
